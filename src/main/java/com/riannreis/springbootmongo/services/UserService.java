e4addc7765cd54fd3694652174efa374f1dac9a1
Riann Silva <ooriannsilva@gmail.com>
2023-04-05 15:32:44 -0300

    Getting an user by id

--- src/main/java/com/riannreis/springbootmongo/services/UserService.java
+++ src/main/java/com/riannreis/springbootmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.riannreis.springbootmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.riannreis.springbootmongo.domain.User;
 import com.riannreis.springbootmongo.repository.UserRepository;
+import com.riannreis.springbootmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
 	}
 }
