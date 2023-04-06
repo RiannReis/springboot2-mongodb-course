32f196d33193aaf0a392756feb36527344c6cb20
Riann Silva <ooriannsilva@gmail.com>
2023-04-06 15:31:06 -0300

    Getting a post by id

---
+++ src/main/java/com/riannreis/springbootmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.riannreis.springbootmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.riannreis.springbootmongo.domain.Post;
+import com.riannreis.springbootmongo.repository.PostRepository;
+import com.riannreis.springbootmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+	}
+}
