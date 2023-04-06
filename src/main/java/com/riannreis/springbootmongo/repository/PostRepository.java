9e03ca5a7f218678134981e2bda9c8f82dc9ecab
Riann Silva <ooriannsilva@gmail.com>
2023-04-05 22:55:13 -0300

    Creating entity Post with nested User

---
+++ src/main/java/com/riannreis/springbootmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.riannreis.springbootmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.riannreis.springbootmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
